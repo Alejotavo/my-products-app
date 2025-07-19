9ba71b1cef04fae64f47fb62d3176038abe04bb9
Alejo <alejogustavof@gmail.com>
2025-07-19 18:00:58 -0300

    refactor user dropdown on header component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,4 @@
-import { Button, Container, Navbar } from "react-bootstrap"
+import { Button, Container, Dropdown, Navbar } from "react-bootstrap"
 import { useAuth } from '../../context/authContext';
 import ThemeToggle from "../themeToggle/themeToggle";
 
@@ -25,18 +25,22 @@ function Header() {
             <div className="mx-3 d-flex">
                 <ThemeToggle />
             </div>
-            <div>
-               <Button onClick={logout} size="sm">Log Out</Button>
-           </div>
-           <div
-              className="d-flex align-items-center justify-content-center rounded-circle bg-secondary text-white fw-medium mx-2"
-              style={{ width: 25, height: 25 }}
-            >
-              {getInitials(user?.username  || '')}
-            </div>
-            <div  className="text-dark fw-medium">
-              {user?.username}
-            </div>
+            <Dropdown>
+              <Dropdown.Toggle variant="light" id="dropdown-basic" className="d-flex align-items-center">
+                <div
+                  className="d-flex align-items-center justify-content-center rounded-circle bg-secondary text-white fw-medium mx-2"
+                  style={{ width: 25, height: 25 }}
+                >
+                  {getInitials(user?.username  || '')}
+                </div>
+                <div  className="text-dark fw-medium">
+                  {user?.username}
+                </div>
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1" onClick={logout}>Log Out</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
           </div>
         </Container>
       </Navbar>
